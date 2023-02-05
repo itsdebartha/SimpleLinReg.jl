417799f5f01f538d2d2a98eb09bcc3bad43d65a0
Debartha Paul <pauldeep102@gmail.com>
2023-02-05 12:38:38 +0530

    Improved Formatting of output
    Also added a test for the function

--- src/SimpleLinReg.jl
+++ src/SimpleLinReg.jl
@@ -8,10 +8,15 @@ include("LinReg.jl")
 
 function Base.show(_io::IO, lreg::LinearRegression)
     io = IOContext(_io, :compact=>get(_io, :compact, true))
+    x = "SIMPLE LINEAR REGRESSION IN TWO VARIABLES:"
+    println(io, x)
+    println(io, repeat("=", length(x)))
     print(io, "Slope:       ")
     show(io, lreg.slope)
+    println(io)
     print(io, "Intercept:   ")
     show(io, lreg.intrcpt)
+    println(io)
 end
 
 export LinearRegression, linreg
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,8 @@ using Test
 x = [1.5,2.3,6.3,5.6,8.1]
 y = [3.2,5.6,9.6,8.7,10.1]
 
+linreg(x,y,false)
+
 @testset "SimpleLinReg.jl" begin
     # Write your tests here.
     @test linreg([3,4],[5,6],true) == LinearRegression(1,2)
