df61776ac0ca208979e86d02ab8f8ba5470e2eb9
Debartha Paul <pauldeep102@gmail.com>
2023-02-05 12:25:54 +0530

    Formatted the output
    Added the output format

--- src/SimpleLinReg.jl
+++ src/SimpleLinReg.jl
@@ -6,6 +6,14 @@ using Statistics
 
 include("LinReg.jl")
 
+function Base.show(_io::IO, lreg::LinearRegression)
+    io = IOContext(_io, :compact=>get(_io, :compact, true))
+    print(io, "Slope:       ")
+    show(io, lreg.slope)
+    print(io, "Intercept:   ")
+    show(io, lreg.intrcpt)
+end
+
 export LinearRegression, linreg
 
 end
