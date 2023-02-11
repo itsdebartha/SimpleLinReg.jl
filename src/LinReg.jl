74269035d388563f237091a4a030dd2d7ee0e28b
Debartha Paul <pauldeep102@gmail.com>
2023-02-11 13:16:22 +0530

    Dimension Mismatch error added

--- src/LinReg.jl
+++ src/LinReg.jl
@@ -9,8 +9,11 @@ end
 Compute the Linear Regression parameters for regression of `y` on `x`.
 
 If `intercept` is unspecified, defaults to `intercept` as `true`.
+
+Mismatching length of vectors `x` and `y` returns `DimensionMismatch`.
 """
 function linreg(x::Vector,y::Vector;intercept::Bool = true)
+    length(x) == length(y) || throw(DimensionMismatch("size of x and y does not match"))
     β = cov(x,y,corrected=false)/var(x,corrected=false)
     if intercept
         α = mean(y)-mean(x)*β
--- test/runtests.jl
+++ test/runtests.jl
@@ -15,4 +15,5 @@ linreg(x,y;intercept = false)
     @test linreg([3,4],[5,6]) == LinearRegression(1,2)
     @test linreg(x,y,intercept = true).slope ≈ 5 atol = 0.2
     @test linreg(x,y,intercept = false).intrcpt == 0
+    @test_throws DimensionMismatch linreg([8,15],[2,4,10])
 end
